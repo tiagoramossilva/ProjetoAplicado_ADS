da45bdd88c6395c23b1cd23fce69842e26f79660
Tiago Ramos Silva <tiagoramossilva40@gmail.com>
2024-11-27 11:01:38 -0300

    historico

--- src/pages/HistoricoCompras/HistoricoCompras.js
+++ src/pages/HistoricoCompras/HistoricoCompras.js
@@ -31,106 +31,100 @@ function HistoricoCompras() {
     fetchData();
   }, []);
 
-  const indexOfLastItem = currentPage * itemsPerPage;
-  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-  const currentItems = compras.slice(indexOfFirstItem, indexOfLastItem); // Usa 'compras' diretamente
-  const totalPages = Math.ceil(compras.length / itemsPerPage); // Usa 'compras' diretamente
-
   return (
     <>
-         <div className="containertitle">
+      <div className="containertitle">
         <div className="divtitle">
           <h1>Histórico de compras</h1>
-        </div>        
+        </div>
       </div>
-    <div className="historico-container">
+      <div className="historico-container">
+        <div className="search-bar">
+          <input
+            className="search-input"
+            type="text"
+            name="fornecedor"
+            placeholder="Fornecedor"
+            value={filters.fornecedor}
+            onChange={handleFilterChange}
+          />
+          <input
+            className="search-input"
+            type="date"
+            name="dataCompra"
+            placeholder="Data da compra"
+            value={filters.dataCompra}
+            onChange={handleFilterChange}
+          />
+          <input
+            className="search-input"
+            type="text"
+            name="valorMin"
+            placeholder="Valor mínimo"
+            value={filters.valorMin}
+            onChange={handleFilterChange}
+          />
+          <input
+            className="search-input"
+            type="text"
+            name="valorMax"
+            placeholder="Valor máximo"
+            value={filters.valorMax}
+            onChange={handleFilterChange}
+          />
+          <button
+            className="filter-button"
+            onClick={() => setData(filteredData)}
+          >
+            Filtrar
+          </button>
+        </div>
 
-      <div className="search-bar">
-        <input
-          className="search-input"
-          type="text"
-          name="fornecedor"
-          placeholder="Fornecedor"
-          value={filters.fornecedor}
-          onChange={handleFilterChange}
-        />
-        <input
-          className="search-input"
-          type="date"
-          name="dataCompra"
-          placeholder="Data da compra"
-          value={filters.dataCompra}
-          onChange={handleFilterChange}
-        />
-        <input
-          className="search-input"
-          type="text"
-          name="valorMin"
-          placeholder="Valor mínimo"
-          value={filters.valorMin}
-          onChange={handleFilterChange}
-        />
-        <input
-          className="search-input"
-          type="text"
-          name="valorMax"
-          placeholder="Valor máximo"
-          value={filters.valorMax}
-          onChange={handleFilterChange}
-        />
-        <button className="filter-button" onClick={() => setData(filteredData)}>
-          Filtrar
-        </button>
-      </div>
-
-      <table className="historico-table">
-        <thead>
-          <tr className="table-header-row">
-            <th className="table-header">Código</th>
-            <th className="table-header">Fornecedor</th>
-            <th className="table-header">Data da Compra</th>
-            <th className="table-header">Valor Total</th>
-            <th className="table-header">Projeto</th>
-            <th className="table-header">GP</th>
-            <th className="table-header">Ações</th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredData.map((row) => (
-            <tr className="table-row" key={row.codigo}>
-              <td className="table-data">{row.codigo}</td>
-              <td className="table-data">{row.fornecedor}</td>
-              <td className="table-data">{row.dataCompra}</td>
-              <td className="table-data">R$ {row.valorTotal}</td>
-              <td className="table-data">{row.projeto}</td>
-              <td className="table-data">{row.gp}</td>
-              <td className="table-actions">
-                <button className="action-button edit-button">
-                  <FaEdit />
-                </button>
-                <button className="action-button delete-button">
-                  <IoTrashBin />
-                </button>
-                <button className="action-button refresh-button">
-                  <FiRefreshCcw />
-                </button>
-              </td>
+        <table className="historico-table">
+          <thead>
+            <tr className="table-header-row">
+              <th className="table-header">Código</th>
+              <th className="table-header">Fornecedor</th>
+              <th className="table-header">Data da Compra</th>
+              <th className="table-header">Valor Total</th>
+              <th className="table-header">Projeto</th>
+              <th className="table-header">Gerente de Projeto</th>
+              <th className="table-header">Ações</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {filteredData.map((row) => (
+              <tr className="table-row" key={row.codigo}>
+                <td className="table-data">{row.codigo}</td>
+                <td className="table-data">{row.fornecedor}</td>
+                <td className="table-data">{row.dataCompra}</td>
+                <td className="table-data">R$ {row.valorTotal}</td>
+                <td className="table-data">{row.projeto}</td>
+                <td className="table-data">{row.gp}</td>
+                <td className="table-actions">
+                  <button className="action-button edit-button">
+                    <FaEdit />
+                  </button>
+                  <button className="action-button delete-button">
+                    <IoTrashBin />
+                  </button>
+                  <button className="action-button refresh-button">
+                    <FiRefreshCcw />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
 
-      <div className="button-container">
-        <button className="cadastrar-button">Cadastrar projeto</button>
-        <button className="cadastrar-button">Cadastrar fornecedor</button>
-        <button
-          className="back-button"
-          onClick={handleBackClick}
-        >
-          Voltar
-        </button>
+        <div className="button-container">
+          <button className="cadastrar-button">Cadastrar projeto</button>
+          <button className="cadastrar-button">Cadastrar fornecedor</button>
+          <button className="back-button" onClick={handleBackClick}>
+            Voltar
+          </button>
+        </div>
       </div>
-    </div>
     </>
   );
 }
