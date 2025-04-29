06c56bdc72fcb76041533fbc7d0379ab43839eb0
STÉFANI LEMOS FERREIRA <stefani_l_ferreira@estudante.sesisenai.org.br>
2025-04-28 21:09:49 -0300

    ajuste de rotas

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,11 +1,18 @@
-import { Link } from 'react-router-dom';
+import { useNavigate  } from 'react-router-dom';
 
 const NotFound = () => {
+  const navigate = useNavigate();
+
+  const handleGoBack = () => {
+    navigate(-1); 
+  };
   return (
     <div style={{ textAlign: 'center', padding: '50px' }}>
       <h1>404 - Página Não Encontrada</h1>
       <p>A página que você está procurando não existe.</p>
-      <Link to="/">Voltar para a página inicial</Link>
+      <button onClick={handleGoBack}>
+        Voltar para a página anterior
+      </button>
     </div>
   );
 };
