d5101348609ed1f2cfd1ed1eaaad00d3b9537d82
Tiago Silva <tiagosilva@Tiago-Ramos-Silvas-MacBook-Pro-ArcTouch-522A.local>
2025-06-17 13:17:05 -0300

    rotas

--- src/services/api.js
+++ src/services/api.js
@@ -8,9 +8,16 @@ export const fetchProdutos = async () => {
 };
 
 export const deleteProduto = async (id) => {
+  const token = localStorage.getItem("token"); // ou como você estiver armazenando
+
   const response = await fetch(`${API_BASE_URL}/produto/${id}`, {
     method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
   });
+
   if (!response.ok) throw new Error("Erro ao deletar produto");
   return true;
 };
